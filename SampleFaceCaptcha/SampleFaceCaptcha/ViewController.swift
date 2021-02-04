95b36ead5a715ffe4b62e5fed2e482a9bf9463ba
Felipe Augusto <felipe@10i9.com.br>
2021-02-04 13:53:46 -0300

    Update sample project to use release 1.2.0.

--- SampleFaceCaptcha/SampleFaceCaptcha/ViewController.swift
+++ SampleFaceCaptcha/SampleFaceCaptcha/ViewController.swift
@@ -10,18 +10,39 @@ import FaceCaptcha
 
 class ViewController: UIViewController {
 
+    /// Armazena FCCameraCapture durante o processo prova de vida.
+    /// Deve ser desalocado nas chamadas do FCCameraCaptureDelegate.
+    private var faceCaptcha: FCCameraCapture?
+
+    /// Trata de clique no botão para abrir o FaceCaptcha
+    /// - Parameter sender: Origem do clique
     @IBAction func openFaceCaptchaPressed(_ sender: Any) {
 
         let fcvarUrlbase = "https://comercial.certiface.com.br:8443/"
 
-        let appKey = ""
+        let appKey = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJjZXJ0aWZhY2UiLCJ1c2VyIjoiRDRBNkNFQjlENzA0RkQwQUJDNzdEMUY0RTVEOTBDMENDQjJDfGZlbGlwZS5tb2JpbGUiLCJlbXBDb2QiOiIwMDAwMDAwMDAxIiwiZmlsQ29kIjoiMDAwMDAwMjU3OCIsImNwZiI6IjM4MDc1NjUwODY3Iiwibm9tZSI6IjRGQTM1OEMwOTAzRDhDMTU4MURFRjdCRTA5ODlFRDZGNjc5NkJGNjk5NTVGMkYxRTUyRDMzRkZDOEZEOTlBQzQxMjcyQURFMEJGRjVFQTJBRTcwM0NERkUwRTc3OEUyMTAwQ0NCMjQyODMyMzc0M0Q2MjdGNDQ4QzVFRkY5ODVFNkNEMUQwMDJBfEZFTElQRSBTSUxWQSIsIm5hc2NpbWVudG8iOiIwNS8wNS8xOTkwIiwiZWFzeS1pbmRleCI6IkFBQUFFdGM5Sy9vUy9sbHZxQm5SdHdqTnNhemtSVjc5OHBGQ005TGpTU2hCN1d4dEp4eURJbkZvUVdMa01nPT0iLCJrZXkiOiJRV0pzWlNCaGJpQm9iM0JsSUc5bUlHSnZaSGt1SUVGdWVTQnVZWGtnYzJoNWJtVT0iLCJleHAiOjE2MTI0NTc3OTIsImlhdCI6MTYxMjQ1NzQ5Mn0.afDm5M55RXy7vbvstG4tj9mW5YpVOO4eEd5Vi6-tG_I"
 
-        /* Modelo USER para envio dos parametos enviando appKey */
         let faceUserModel = FCUserModel("", "", "", "", "", UIImage(named: "overlay"))
 
-        /* Abrir a camera com o overlapoyview que definir */
-        let faceCamera = FCCameraCapture(model: faceUserModel, appkey: "\(appKey)", fcvarUrlbase: "\(fcvarUrlbase)", viewController: self, delegate: self)
-        faceCamera.show()
+        faceCaptcha = FCCameraCapture(model: faceUserModel,
+                                      appkey: appKey,
+                                      fcvarUrlbase: fcvarUrlbase,
+                                      viewController: self,
+                                      delegate: self,
+                                      showSetupErrors: false)
+        faceCaptcha?.show()
+    }
+
+    /// Exibe um UIAlertController.
+    /// - Parameters:
+    ///   - title: Título da alerta
+    ///   - message: Mensagem do alerta
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.present(alert, animated: true, completion: nil)
+        }
     }
 }
 
@@ -29,43 +50,23 @@ class ViewController: UIViewController {
 
 extension ViewController: FCCameraCaptureDelegate {
 
-    /// Callback se a API do certiface estiver indisponível ou app key inválido
-    /// certiface indisponível error = certfaceoff
-    /// appkey inválido error = appkeyinvalido
-    /// - Parameters:
-    ///   - error:
-    ///   - imageBase64: uma unica imagem base64
-    func handleCaptureVideoError(_ error: String, _ imageBase64: String) {
-        debugPrint("ERROR: \(error)")
-        debugPrint("BASE64: \(imageBase64)")
+    /// Callback chamada em caso de desafio concluído.
+    /// - Parameter validateModel: Modelo para verificação se prova de vida obteve sucesso
+    func handleCaptureValidation(_ validateModel: FCValidCaptchaModel) {
+        debugPrint("handleCaptureValidation: \(validateModel)")
+        faceCaptcha = nil
+        showAlert(title: "Desafio concluído",
+                  message: "Válido: \(validateModel.valid ?? false)\nMotivo: \(validateModel.cause ?? "")")
     }
 
-    /// Callback do serviço quando termina de processar e separar os frames do video.
-    ///
+    /// Callback chamada em caso de erro durante execução do FaceCaptcha.
     /// - Parameters:
-    ///   - fcCameraCapture: FCCameraCapture Object para efetuar a chamada do validate.
-    ///   - fcImages: Frames do video para envio nos parametros necessários.
-    func handleCaptureVideo(_ fcCameraCapture: FCCameraCapture, _ fcImages: [UIImage]) {
-
-        /* Chamar o serviço de validação passando os frames de parametro */
-        fcCameraCapture.validate(fcImages, { (fcValidateModel, error) in
-
-            if let error = error {
-                debugPrint("ERROR: \(error)")
-            }
-
-            guard let fcValidateModel = fcValidateModel else {
-                debugPrint("ERROR: fcValidateModel not loaded")
-                return
-            }
-
-            debugPrint("SUCCESS: \(fcValidateModel)")
-
-            let alert = UIAlertController(title: "Resultado",
-                                          message: "Válido: \(fcValidateModel.valid ?? false)\nMotivo: \(fcValidateModel.cause ?? "")",
-                                          preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
-        })
+    ///   - error: Erro ocorrido
+    ///   - imageBase64: imagem capturada
+    func handleCaptureVideoError(_ error: FaceCaptchaError, imageBase64: String?) {
+        debugPrint("handleCaptureVideoError: \(error)")
+        faceCaptcha = nil
+        showAlert(title: "Erro",
+                  message: "\(error)")
     }
 }
